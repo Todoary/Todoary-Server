d6cf5adcea7654bb02df996a822f593e9ccd1bad
KyeonJong-Yoo <rudwhd515@gmail.com>
2022-07-11 18:07:32 +0900

    [Rename] directory todoary/~ -> ~/

--- todoary/src/main/java/com/ms/umc/todoary/src/login/LoginController.java
+++
@@ -1,4 +0,0 @@
-package com.ms.umc.todoary.src.login;
-
-public class LoginController {
-}
--- todoary/src/main/java/com/ms/umc/todoary/src/user/UserProvider.java
+++
@@ -1,9 +0,0 @@
-package com.ms.umc.todoary.src.user;
-
-import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-
-@Service
-public class UserProvider {
-}
