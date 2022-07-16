94e76257c25c0904e9b8b8601d299090e88c190d
ggyul <wakegyul@gmail.com>
2022-07-16 21:30:59 +0900

    [Chore] Add Empty configuration file `application.yml`
    [Fix] Set filter url to `/auth/signin`

--- src/main/java/com/ms/umc/todoary/config/SecurityConfig.java
+++ src/main/java/com/ms/umc/todoary/config/SecurityConfig.java
@@ -16,8 +16,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -41,7 +39,7 @@ public class SecurityConfig {
     public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
         AuthenticationManager authenticationManager = http.getSharedObject(AuthenticationManager.class);
         JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(jwtService, authenticationManagerBuilder);
-        jwtAuthenticationFilter.setFilterProcessesUrl("/auth/login");
+        jwtAuthenticationFilter.setFilterProcessesUrl("/auth/signin");
 
         http
                 .addFilter(jwtAuthenticationFilter)
