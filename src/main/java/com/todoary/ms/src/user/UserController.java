75187940af497070b9c3918680a3e5e92e9e1257
wonseok <tkddls23@konkuk.ac.kr>
2022-07-22 04:37:51 +0900

    [Feat] Add Patch password api

--- src/main/java/com/todoary/ms/src/user/UserController.java
+++ src/main/java/com/todoary/ms/src/user/UserController.java
@@ -1,6 +1,7 @@
 package com.todoary.ms.src.user;
 
 import com.todoary.ms.src.user.dto.GetUserRes;
+import com.todoary.ms.src.user.dto.PatchPasswordReq;
 import com.todoary.ms.src.user.dto.PatchUserReq;
 import com.todoary.ms.src.user.dto.PatchUserRes;
 import com.todoary.ms.src.user.model.User;
@@ -30,6 +31,23 @@ public class UserController {
         this.userProvider = userProvider;
     }
 
+    /**
+     * 2.1 비밀번호 재설정 API
+     *
+     * @param request
+     * @return
+     */
+    @PatchMapping("/password")
+    public BaseResponse<BaseResponseStatus> patchUserPassword(HttpServletRequest request, @RequestBody PatchPasswordReq patchPasswordReq){
+        try{
+            Long user_id = Long.parseLong(request.getAttribute("user_id").toString());
+            String Password = userProvider.retrieveById(user_id).getPassword();
+            userService.changePassword(user_id,Password,patchPasswordReq);
+            return new BaseResponse<>(BaseResponseStatus.SUCCESS);
+        } catch (BaseException e) {
+            return new BaseResponse<>(e.getStatus());
+        }
+    }
 
     /**
      * 2.5 프로필 조회 api
