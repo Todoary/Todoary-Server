842254cc7bd5a143d498fdb04b5a5cfe2cc6b660
JUWON <juwonlee0509@gmail.com>
2022-07-22 03:25:15 +0900

    [feat] : Add_ onoff_alarm&service_terms

---
+++ src/main/java/com/todoary/ms/src/user/dto/PatchDiaryAlarmReq.java
@@ -0,0 +1,12 @@
+package com.todoary.ms.src.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class PatchDiaryAlarmReq {
+
+    private boolean isChecked;
+
+}
---
+++ src/main/java/com/todoary/ms/src/user/dto/PatchTodoaryAlarmReq.java
@@ -0,0 +1,12 @@
+package com.todoary.ms.src.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class PatchTodoaryAlarmReq {
+
+    private boolean isChecked;
+
+}
