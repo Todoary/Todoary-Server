bc3438868f58bcb575b196d8f6c8eccf01c4ec72
wonseok <tkddls4456@gmail.com>
2022-07-28 18:40:53 +0900

    [Fix] Alarm-Todo-Controller

--- src/main/java/com/todoary/ms/src/todo/TodoController.java
+++ src/main/java/com/todoary/ms/src/todo/TodoController.java
@@ -49,8 +49,6 @@ public class TodoController {
         try {
             long userId = getUserIdFromRequest(request);
             long todoId = todoService.createTodo(userId, postTodoReq);
-            if (postTodoReq.isAlarmEnabled())
-                alarmService.createAlarmTodo(userId, todoId);
             return new BaseResponse<>(new PostTodoRes(todoId));
         } catch (BaseException e) {
             log.warn(e.getMessage());
@@ -73,8 +71,6 @@ public class TodoController {
         try {
             long userId = getUserIdFromRequest(request);
             todoService.modifyTodo(userId, todoId, postTodoReq);
-            if (!postTodoReq.isAlarmEnabled())
-                alarmService.modifyAlarmTodo(userId, todoId);
             return new BaseResponse<>(BaseResponseStatus.SUCCESS);
         } catch (BaseException e) {
             log.warn(e.getMessage());
