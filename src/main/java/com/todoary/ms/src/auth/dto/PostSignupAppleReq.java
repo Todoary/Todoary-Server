02cc0642e4b224019a24905cf69699076b53a16d
wonseok <tkddls23@konkuk.ac.kr>
2022-08-24 17:18:37 +0900

    [Feat] : Auth-Apple-Server

--- src/main/java/com/todoary/ms/src/auth/dto/PostSignupAppleReq.java
+++ src/main/java/com/todoary/ms/src/auth/dto/PostSignupAppleReq.java
@@ -1,5 +1,6 @@
 package com.todoary.ms.src.auth.dto;
 
+import com.todoary.ms.src.auth.model.AppleUserInfo;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,4 +11,5 @@ import lombok.NoArgsConstructor;
 public class PostSignupAppleReq {
     private String code;
     private String idToken;
+    private AppleUserInfo appleUserInfo;
 }
