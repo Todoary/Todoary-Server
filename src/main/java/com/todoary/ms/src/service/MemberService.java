66611bb8134db7cabe2f2ac6a58f7e004cabfb05
60jong <rudwhd515@gmail.com>
2023-01-17 17:21:08 +0900

    feat : MemberService 구현 및 테스트 케이스 작성

---
+++ src/main/java/com/todoary/ms/src/service/MemberService.java
@@ -0,0 +1,33 @@
+package com.todoary.ms.src.service;
+
+import com.todoary.ms.src.domain.Member;
+import com.todoary.ms.src.domain.token.RefreshToken;
+import com.todoary.ms.src.repository.MemberRepository;
+import com.todoary.ms.src.web.dto.MemberJoinRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    public Long join(Member newMember) {
+        return memberRepository.save(newMember);
+    }
+
+    public Boolean existsById(Long memberId) {
+        return memberRepository.existById(memberId);
+    }
+
+    public Member findById(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow();
+    }
+
+    public Boolean existsByRefreshToken(RefreshToken refreshToken) {
+        return memberRepository.existByRefreshToken(refreshToken);
+    }
+}
---
+++ src/test/java/com/todoary/ms/src/service/MemberServiceTest.java
@@ -0,0 +1,46 @@
+package com.todoary.ms.src.service;
+
+import com.todoary.ms.src.domain.Member;
+import com.todoary.ms.src.repository.MemberRepository;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Transactional
+@SpringBootTest
+class MemberServiceTest {
+    @Autowired
+    EntityManager em;
+
+    @Autowired
+    MemberService memberService;
+
+    @MockBean
+    MemberRepository memberRepository;
+
+    @Test
+    public void 멤버_회원가입() throws Exception {
+        //given
+        Member member = createMember();
+
+        //when
+        Long joinMemberId = memberService.join(member);
+
+        //then
+        assertThat(joinMemberId).isEqualTo(1L);
+    }
+
+    Member createMember() {
+        Member member = Member.builder().build();
+        em.persist(member);
+        Mockito.when(memberRepository.save(member)).thenReturn(member.getId());
+        return member;
+    }
+}
