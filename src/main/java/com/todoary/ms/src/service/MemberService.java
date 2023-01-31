4cb51eb67076a47f96d93fe00e6066ec684be8ff
60jong <rudwhd515@gmail.com>
2023-01-31 19:36:12 +0900

    feat : 일반 회원가입 기능 구현

--- src/main/java/com/todoary/ms/src/service/MemberService.java
+++ src/main/java/com/todoary/ms/src/service/MemberService.java
@@ -2,13 +2,18 @@ package com.todoary.ms.src.service;
 
 import com.todoary.ms.src.auth.jwt.JwtTokenProvider;
 import com.todoary.ms.src.domain.Member;
+import com.todoary.ms.src.domain.Provider;
+import com.todoary.ms.src.domain.ProviderAccount;
 import com.todoary.ms.src.domain.token.RefreshToken;
 import com.todoary.ms.src.exception.common.TodoaryException;
 import com.todoary.ms.src.repository.MemberRepository;
-import com.todoary.ms.util.BaseResponseStatus;
+import com.todoary.ms.src.web.dto.MemberJoinParam;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import static com.todoary.ms.util.BaseResponseStatus.*;
 
 @Service
 @RequiredArgsConstructor
@@ -16,8 +21,19 @@ public class MemberService {
     private final MemberRepository memberRepository;
     private final JwtTokenProvider jwtTokenProvider;
 
+    private final PasswordEncoder passwordEncoder;
+
     @Transactional
-    public Long join(Member newMember) {
+    public Long join(MemberJoinParam memberJoinParam) {
+        Member newMember = Member.builder()
+                .name(memberJoinParam.getName())
+                .nickname(memberJoinParam.getNickname())
+                .email(memberJoinParam.getEmail())
+                .password(memberJoinParam.getPassword())
+                .role(memberJoinParam.getRole())
+                .providerAccount(new ProviderAccount(Provider.NONE, "none"))
+                .isTermsEnable(memberJoinParam.isTermsEnable())
+                .build();
         return memberRepository.save(newMember);
     }
 
@@ -27,7 +43,7 @@ public class MemberService {
 
     public Member findById(Long memberId) {
         return memberRepository.findById(memberId)
-                .orElseThrow(() -> new TodoaryException(BaseResponseStatus.USERS_DELETED_USER));
+                .orElseThrow(() -> new TodoaryException(USERS_DELETED_USER));
     }
 
     public Boolean existsByRefreshToken(RefreshToken refreshToken) {
@@ -40,7 +56,21 @@ public class MemberService {
         Member findMember = findById(memberId);
 
         if (!findMember.hasRefreshTokenCode(refreshTokenCode)) {
-            throw new TodoaryException(BaseResponseStatus.INVALID_JWT);
+            throw new TodoaryException(INVALID_JWT);
         }
+    }
+
+    public String encodePassword(String password) {
+        return passwordEncoder.encode(password);
+    }
+
+    public Member findByEmail(String email) {
+        return memberRepository.findByEmail(email).orElseThrow(
+                () -> new TodoaryException(USERS_EMPTY_USER_EMAIL));
+    }
+
+    public Member findByProviderEmail(String email, String providerName) {
+        return memberRepository.findByProviderEmail(email, providerName).orElseThrow(
+                () -> new TodoaryException(USERS_EMPTY_USER_EMAIL));
     }
 }
