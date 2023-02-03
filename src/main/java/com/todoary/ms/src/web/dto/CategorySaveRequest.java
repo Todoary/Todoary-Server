a26af26567256cac4dc0674eba1a0774fbf9eefe
ggyul <wakegyul@gmail.com>
2023-02-03 20:48:14 +0900

    feat: CategorySaveRequest 필드 검증하도록 어노테이션 추가한다

--- src/main/java/com/todoary/ms/src/web/dto/CategorySaveRequest.java
+++ src/main/java/com/todoary/ms/src/web/dto/CategorySaveRequest.java
@@ -6,12 +6,20 @@ import com.todoary.ms.src.domain.Member;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotNull;
+
+import static com.todoary.ms.util.ColumnLengthInfo.CATEGORY_TITLE_MAX_LENGTH;
 
 @Getter @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
 public class CategorySaveRequest {
+    @Length(max = CATEGORY_TITLE_MAX_LENGTH, message="TITLE_TOO_LONG")
     private String title;
+    @NotNull(message = "EMPTY_COLOR_CATEGORY")
     private Integer color;
-
     public CategorySaveRequest(String title, Integer color) {
         this.title = title;
         this.color = color;
