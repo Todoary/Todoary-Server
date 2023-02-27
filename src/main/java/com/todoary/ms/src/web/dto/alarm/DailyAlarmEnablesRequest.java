297e7d950c5047c58137e688e32abe153b88c8a0
60jong <rudwhd515@gmail.com>
2023-02-27 17:37:34 +0900

    fix : /member/alarm/diary req dto에 @JsonPropety 추가

--- src/main/java/com/todoary/ms/src/web/dto/alarm/DailyAlarmEnablesRequest.java
+++ src/main/java/com/todoary/ms/src/web/dto/alarm/DailyAlarmEnablesRequest.java
@@ -1,5 +1,6 @@
 package com.todoary.ms.src.web.dto.alarm;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,5 +9,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class DailyAlarmEnablesRequest {
+    @JsonProperty("isChecked")
     private boolean isChecked;
 }
