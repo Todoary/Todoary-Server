cab1b91caae40b485a832a7b275e85a0e09abe72
ggyul <wakegyul@gmail.com>
2023-02-28 21:23:49 +0900

    chore: 새로운 서버로 교체 시 교체 후 기존 서버 종료한다

--- service-scripts/stop.sh
+++ service-scripts/stop.sh
@@ -4,13 +4,16 @@ ABSPATH=$(readlink -f $0)
 ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
-IDLE_PORT=$(find_idle_port)
+if [ $# -ge 1 ]; then
+  IDLE_PORT=$1
+else
+  IDLE_PORT=$(find_idle_port)
+fi
 
 echo "> $IDLE_PORT 에서 구동 중인 애플리케이션 pid 확인"
 IDLE_PID=$(lsof -ti tcp:"$IDLE_PORT")
 
-if [ -z ${IDLE_PID} ]
-then
+if [ -z ${IDLE_PID} ]; then
   echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -15 $IDLE_PID"
