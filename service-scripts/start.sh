0132b88da595f3e9b20a8cf27eeab212fd8db099
ggyul <wakegyul@gmail.com>
2023-02-28 13:26:55 +0900

    chore: 서버 로그 파일 이름명 변경한다

--- service-scripts/start.sh
+++ service-scripts/start.sh
@@ -28,4 +28,4 @@ echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 
 nohup java -jar \
     -Dspring.profiles.active=$IDLE_PROFILE \
-    $JAR_NAME > $REPOSITORY/nohup$IDLE_PROFILE.out 2>&1 &
+    $JAR_NAME > $REPOSITORY/$IDLE_PROFILE.out 2>&1 &
