5d66c2a26dcd38a4a9069ef5db88aad52ad83d9f
ggyul <wakegyul@gmail.com>
2023-02-28 15:19:08 +0900

    fix: api 명세서에 맞게 에러코드 수정한다

--- src/main/java/com/todoary/ms/src/common/response/BaseResponseStatus.java
+++ src/main/java/com/todoary/ms/src/common/response/BaseResponseStatus.java
@@ -19,6 +19,7 @@ public enum BaseResponseStatus {
      * 2000 : Request 오류
      */
     // Common
+    EMPTY_USER(false, 2001, "회원 정보가 존재하지 않습니다."),
     INVALID_JWT(false, 2002, "유효하지 않은 JWT입니다."),
     EXPIRED_JWT(false, 2003, "만료된 JWT입니다."),
     INVALID_USER_JWT(false, 2004, "권한이 없는 유저의 접근입니다."),
@@ -30,15 +31,10 @@ public enum BaseResponseStatus {
     INVALID_APPLE_AUTH(false, 2008, "유효하지 않은 토큰 입니다."),
     PARSE_USER_ERROR(false, 2009, "애플 유저 조회에 실패하였습니다."),
 
-    EMPTY_USER(false, 2010, "회원 정보가 존재하지 않습니다."),
-
     // users
     USERS_EMPTY_USER_ID(false, 2010, "유저 아이디 값을 확인해주세요."),
     USERS_EMPTY_USER_EMAIL(false, 2011, "유저 이메일 값을 확인해주세요."),
     USERS_DELETED_USER(false, 2012, "삭제된 유저입니다."),
-
-    ILLEGAL_ARGUMENT(false, 2013, "잘못된 입력입니다."),
-    ILLEGAL_DATETIME(false, 2014, "날짜나 시간 형식을 확인해주세요."),
 
     NULL_ARGUMENT(false, 2015, "입력값이 있어야 합니다."),
     MEMBERS_DUPLICATE_NICKNAME(false, 2032, "중복된 닉네임입니다."),
@@ -64,13 +60,13 @@ public enum BaseResponseStatus {
     // 300대 -> 3. todo 도메인 에러
     USERS_CATEGORY_NOT_EXISTS(false, 2301, "해당하는 유저와 일치하는 카테고리가 없습니다."),
     USERS_TODO_NOT_EXISTS(false, 2302, "해당하는 유저와 일치하는 투두가 없습니다."),
-    TODO_TITLE_TOO_LONG(false, 2303, "투두 제목이 제한길이 이상입니다. ("+TODO_TITLE_MAX_LENGTH+" 글자까지 가능)"),
+    TODO_TITLE_TOO_LONG(false, 2403, "투두 제목이 제한길이 이상입니다. ("+TODO_TITLE_MAX_LENGTH+" 글자까지 가능)"),
     EMPTY_TODO_DATE(false, 2304, "투두 날짜를 입력해주세요."),
 
     // diary 도메인 에러
 
     USERS_DIARY_NOT_EXISTS(false, 2402, "해당하는 유저와 일치하는 일기가 없습니다."),
-    DIARY_TITLE_TOO_LONG(false, 2403, "일기 제목이 제한길이 이상입니다. ("+DIARY_TITLE_MAX_LENGTH+" 글자까지 가능)"),
+    DIARY_TITLE_TOO_LONG(false, 2303, "일기 제목이 제한길이 이상입니다. ("+DIARY_TITLE_MAX_LENGTH+" 글자까지 가능)"),
     EMPTY_DIARY_DATE(false, 2404, "일기 날짜를 입력해주세요."),
     EMPTY_STICKER_TYPE(false, 2405, "스티커 타입(스티커 종류 번호)을 입력해주세요."),
     EMPTY_STICKER_ID(false, 2406, "스티커 아이디(스티커 고유 번호)를 입력해주세요."),
@@ -86,6 +82,8 @@ public enum BaseResponseStatus {
     DATABASE_ERROR(false, 4000, "데이터베이스 연결에 실패하였습니다."),
 
     INTERNAL_SERVER_ERROR(false, 4004, "서버에서 에러가 발생했습니다"),
+    ILLEGAL_ARGUMENT(false, 4005, "잘못된 입력입니다."),
+    ILLEGAL_DATETIME(false, 4006, "날짜나 시간 형식을 확인해주세요."),
 
     //[PATCH] /users/{userIdx}
     MODIFY_FAIL_FCMTOKEN(false, 4015, "FCM Token 수정 실패"),
